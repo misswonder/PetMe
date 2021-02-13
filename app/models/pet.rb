ab265d9ae773e116bff4c28a4b49ac9986eb1c31
puifaiba <62617396+puifaiba@users.noreply.github.com>
2021-02-13 00:32:36 -0500

    Revert "first commit"

--- app/models/pet.rb
+++
@@ -1,5 +0,0 @@
-class Pet < ApplicationRecord
-  belongs_to :breed
-  has_many :profiles, dependent: :destroy 
-  has_many :users, through: :profiles
-end
--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ApplicationRecord
-    has_many :profiles, dependent: :destroy 
-    has_many :pets, through: :profiles
-end
