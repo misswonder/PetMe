a65e3e109a366af81e75d8f68bc1f0552bcea440
Yvonne Chen <yvonnechen1991@gmail.com>
2021-02-17 16:35:41 -0600

    day two

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,7 +1,7 @@
 class PetsController < ApplicationController
 
     def index
-        @pets = Pet.all
+        @pets = Pet.get_dogs
     end
 
     def show 
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,22 @@ class Pet < ApplicationRecord
   belongs_to :breed
   has_many :profiles, dependent: :destroy 
   has_many :users, through: :profiles
+
+  Petfinder.configure do |config|
+    config.api_key = 'ESVgrWdR9xyGTq5IuJYlb5FJ6JhZYWb5pUyY7zx5ozrttfgHYS'
+    config.api_secret = 'FrA4yn5xeK9O3dAnDKw1uG62fPMnDP38PG53LuXQ'
+  end
+  
+  PETFINDER = Petfinder::Client.new
+  
+  def self.get_dogs
+    @pets = PETFINDER.animals(type: 'dog', location: '90210')
+  end
+
+
+
+  # def self.get_pet
+  #   @pet = petfinder.animals.first.name
+  # end
+
 end
