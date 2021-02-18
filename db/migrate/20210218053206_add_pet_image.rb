7ea7ff2c0f2b20d6a718c4f4fd322eaac8a570a7
Yvonne Chen <yvonnechen1991@gmail.com>
2021-02-18 00:14:55 -0600

    seed image

---
+++ db/migrate/20210218053206_add_pet_image.rb
@@ -0,0 +1,5 @@
+class AddPetImage < ActiveRecord::Migration[6.1]
+  def change
+    add_column :pets, :image, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_175957) do
+ActiveRecord::Schema.define(version: 2021_02_18_053206) do
 
   create_table "breeds", force: :cascade do |t|
     t.string "species"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_02_16_175957) do
     t.string "temperament"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
     t.index ["breed_id"], name: "index_pets_on_breed_id"
   end
 
